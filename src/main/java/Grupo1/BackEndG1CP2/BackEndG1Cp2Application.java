8310c70e909fcf59352621c0709e52f4f4cfa80f
Kevin Aguilar <kevinv.aguilarl@gmail.com>
2022-06-03 20:31:43 -0500

    V1.3.6

---
+++ src/main/java/Grupo1/BackEndG1CP2/BackEndG1Cp2Application.java
@@ -0,0 +1,13 @@
+package Grupo1.BackEndG1CP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndG1Cp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndG1Cp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Grupo1/BackEndG1CP2/BackEndG1Cp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo1.BackEndG1CP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndG1Cp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
