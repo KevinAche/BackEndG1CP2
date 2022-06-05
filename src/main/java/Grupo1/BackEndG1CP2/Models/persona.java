05e035ce7c1bacc418cd334e261ae044537e62a6
Kevin Aguilar <kevinv.aguilarl@gmail.com>
2022-06-04 21:14:53 -0500

    CONEXION EN PROPERTIES Y NUEVOS ATRIBUTOS EN MODELO PERSONA

--- src/main/java/Grupo1/BackEndG1CP2/Models/persona.java
+++ src/main/java/Grupo1/BackEndG1CP2/Models/persona.java
@@ -1,6 +1,7 @@
 package Grupo1.BackEndG1CP2.Models;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "persona")
@@ -9,9 +10,120 @@ public class persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nombre;
+
+
+    @Column(length = 10)
+    private String cedula;
+
+    @Column(length = 60)
+    private  String nombres;
+
+    @Column(length = 60)
     private String apellidos;
+
+    @Column(name = "fecha_nac")
+    private Date fecha_nac;
+
+    @Column(length = 15)
+    private String telefono;
+
+    @Column(length = 150)
     private String direccion;
-    //comentario
-    //descomentados
+
+    @Column(length = 150)
+    private String correo;
+
+    public persona() {
+    }
+
+    public persona(Long id, String cedula, String nombres, String apellidos, Date fecha_nac, String telefono, String direccion, String correo) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fecha_nac = fecha_nac;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.correo = correo;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(Date fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+
+    @Override
+    public String toString() {
+        return "persona{" +
+                "id=" + id +
+                ", cedula='" + cedula + '\'' +
+                ", nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", fecha_nac=" + fecha_nac +
+                ", telefono='" + telefono + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
 }
