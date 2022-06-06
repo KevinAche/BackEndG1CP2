174399c42947790f11fc4ec724ca4e5fb2e78004
xanderez17 <kevxanderez17@gmail.com>
2022-06-06 18:06:37 -0500

    Usuarios - Roles
    Relación Usuario-Persona-Rol

---
+++ src/main/java/Grupo1/BackEndG1CP2/Models/Rol.java
@@ -0,0 +1,42 @@
+package Grupo1.BackEndG1CP2.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Rol {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idRol;
+	
+	@Column(nullable = false)
+	private String rol;
+
+	public Rol() {
+		super();
+	}
+
+	public Long getIdRol() {
+		return idRol;
+	}
+
+	public void setIdRol(Long idRol) {
+		this.idRol = idRol;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	
+}
---
+++ src/main/java/Grupo1/BackEndG1CP2/Models/Usuario.java
@@ -0,0 +1,79 @@
+package Grupo1.BackEndG1CP2.Models;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUsuario;
+	
+	private String username;
+	
+	private String password;
+	
+	@ManyToOne
+	@JoinColumn(name = "idPersona", referencedColumnName = "idPersona")
+	private Persona persona;
+	
+	@ManyToMany
+	@JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "rol_id"))
+	private List<Rol> rol;
+
+	public Usuario() {
+		super();
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Persona getPersona() {
+		return persona;
+	}
+
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+
+	public List<Rol> getRol() {
+		return rol;
+	}
+
+	public void setRol(List<Rol> rol) {
+		this.rol = rol;
+	}
+	
+}
