62423d98e9892362e7cd4f3d4cb116d336ed9dce
Kevin Aguilar <kevinv.aguilarl@gmail.com>
2022-06-08 21:16:50 -0500

    CREACION ALUMNNO CON ID PERSONA Y ID EMPRESA

--- src/main/java/Grupo1/BackEndG1CP2/Controllers/AlumnoController.java
+++ src/main/java/Grupo1/BackEndG1CP2/Controllers/AlumnoController.java
@@ -1,10 +1,12 @@
 package Grupo1.BackEndG1CP2.Controllers;
 
 import Grupo1.BackEndG1CP2.Models.Alumno;
+import Grupo1.BackEndG1CP2.Models.Carrera;
 import Grupo1.BackEndG1CP2.Models.Persona;
 import Grupo1.BackEndG1CP2.Models.RespuestaGenerica;
 import Grupo1.BackEndG1CP2.Models.Views.VistaListarAlumnos;
 import Grupo1.BackEndG1CP2.Repositories.AlumnoRepository;
+import Grupo1.BackEndG1CP2.Repositories.CarreraRepository;
 import Grupo1.BackEndG1CP2.Repositories.PersonaRepository;
 import Grupo1.BackEndG1CP2.Repositories.ViewRepositories.ListarAlumnosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,9 @@ public class AlumnoController {
     @Autowired
     private PersonaRepository personaRepository;
 
+    @Autowired
+    private CarreraRepository carreraRepository;
+
 
 
     @GetMapping("/ListaAlumnos")
@@ -49,23 +54,32 @@ public class AlumnoController {
     }
 
 
-    @PostMapping("/CrearAlumno/{id_persona}")
-    public ResponseEntity<RespuestaGenerica> CrearAlumno(@RequestBody Alumno alumnoEnviado,@PathVariable Long id_persona){
+    @PostMapping("/CrearAlumno/{id_persona}/{id_carrera}")
+    public ResponseEntity<RespuestaGenerica> CrearAlumno(@RequestBody Alumno alumnoEnviado,@PathVariable Long id_persona,@PathVariable Long id_carrera){
         List<Alumno> data = new ArrayList<>();
         RespuestaGenerica<Alumno> respuesta = new RespuestaGenerica<>();
         HttpStatus estado  = HttpStatus.CREATED;
         try {
             Optional<Persona> persona = personaRepository.findById(id_persona);
             if(!persona.isEmpty()){
-                alumnoEnviado.setPersona(persona.get());
-                Alumno alumno = alumnoRepository.save(alumnoEnviado);
-                data.add(alumno);
-                if(alumno !=null){
-                    respuesta.setMensaje("SE REGISTRO ALUMNO CORRECTAMENTE");
-                    respuesta.setData(data);
-                    respuesta.setEstado(0);
+                Optional<Carrera> carrera = carreraRepository.findById(id_carrera);
+                if(!carrera.isEmpty()){
+                    alumnoEnviado.setCarrera(carrera.get());
+                    alumnoEnviado.setPersona(persona.get());
+                    Alumno alumno = alumnoRepository.save(alumnoEnviado);
+                    data.add(alumno);
+                    if(alumno !=null){
+                        respuesta.setMensaje("SE REGISTRO ALUMNO CORRECTAMENTE");
+                        respuesta.setData(data);
+                        respuesta.setEstado(0);
+                    }else{
+                        respuesta.setMensaje("NO SE REGISTRO ALUMNO CORRECTAMENTE");
+                        respuesta.setData(data);
+                        respuesta.setEstado(1);
+                        estado= HttpStatus.BAD_REQUEST;
+                    }
                 }else{
-                    respuesta.setMensaje("NO SE REGISTRO ALUMNO CORRECTAMENTE");
+                    respuesta.setMensaje("NO SE REGISTRO ALUMNO CORRECTAMENTE DEBIDO A QUE EL ID DE CARRERA ENVIADO NO SE ENCONTRO EN LA BASE");
                     respuesta.setData(data);
                     respuesta.setEstado(1);
                     estado= HttpStatus.BAD_REQUEST;
@@ -85,5 +99,32 @@ public class AlumnoController {
         return new ResponseEntity<RespuestaGenerica>(respuesta, estado);
     }
 
+  /*  @PostMapping("/EditarAlumno/{cedula}")
+    public ResponseEntity<RespuestaGenerica> EditarAlumno(@RequestBody Alumno alumnoEnviado,@PathVariable String cedula){
+        List<Alumno> data = new ArrayList<>();
+        RespuestaGenerica<Alumno> respuesta = new RespuestaGenerica<>();
+        HttpStatus estado  = HttpStatus.CREATED;
+        try {
+            Persona persona = personaRepository.findByCedula(cedula);
+            alumnoEnviado.setPersona(persona);
+
+            if(persona!= null){
+
+
+            }else{
+                respuesta.setMensaje("Hubo un problema al editar ALUMNO,DEDIDO A QUE LA CEDULA INGRESADA NO FUE ENCONTRADA");
+                respuesta.setData(data);
+                respuesta.setEstado(1);
+                estado= HttpStatus.BAD_REQUEST;
+            }
+        }catch (Exception e){
+            respuesta.setMensaje("Hubo un problema al insertar ALUMNO, causa ->"+e.getCause()+ " || message -> "+e.getMessage());
+            respuesta.setData(data);
+            respuesta.setEstado(1);
+            estado= HttpStatus.BAD_REQUEST;
+        }
+        return new ResponseEntity<RespuestaGenerica>(respuesta, estado);
+    }
+*/
 
 }
--- src/main/java/Grupo1/BackEndG1CP2/Controllers/EmpresaController.java
+++ src/main/java/Grupo1/BackEndG1CP2/Controllers/EmpresaController.java
@@ -1,15 +1,44 @@
 package Grupo1.BackEndG1CP2.Controllers;
 
+import Grupo1.BackEndG1CP2.Models.Empresa;
+import Grupo1.BackEndG1CP2.Models.Persona;
+import Grupo1.BackEndG1CP2.Models.RespuestaGenerica;
 import Grupo1.BackEndG1CP2.Repositories.ActividadesRepository;
 import Grupo1.BackEndG1CP2.Repositories.EmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/Empresa")
+@RequestMapping("/GestionEmpresa")
 public class EmpresaController {
 
     @Autowired
     private EmpresaRepository empresaRepository;
+
+    @GetMapping("/ListaEmpresas")
+    public ResponseEntity<RespuestaGenerica> ListarEmpresas(){
+        List<Empresa> data = new ArrayList<>();
+        RespuestaGenerica<Empresa> respuesta = new RespuestaGenerica<>();
+        try {
+            data=empresaRepository.findAll();
+            respuesta.setMensaje("Se genero LISTADO EMPRESAS EXITOSAMENTE");
+            respuesta.setData(data);
+            respuesta.setEstado(0);
+        }catch (Exception e){
+            respuesta.setMensaje("Hubo un problema al generar LISTADO PERSONAS, causa ->"+e.getCause()+" || messagge->"+e.getMessage());
+            respuesta.setData(data);
+            respuesta.setEstado(1);
+        }
+        return new ResponseEntity<RespuestaGenerica>(respuesta, HttpStatus.OK);
+    }
+
+
+
 }
