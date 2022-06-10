bbbfd1bcb96782e488199ef4db31861732bf19fa
myusername <98288187+xanderez17@users.noreply.github.com>
2022-06-09 20:40:44 -0500

    PERSONALEMPRESA CONTROLLER FUNCIONAL
    CRUD COMPLETO FUNCIONAL

--- src/main/java/Grupo1/BackEndG1CP2/Controllers/PersonalEmpresaController.java
+++ src/main/java/Grupo1/BackEndG1CP2/Controllers/PersonalEmpresaController.java
@@ -17,14 +17,17 @@ import Grupo1.BackEndG1CP2.Repositories.ViewRepositories.ListarPersonalRepositor
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,7 +65,6 @@ public class PersonalEmpresaController {
        }
        return  new ResponseEntity<RespuestaGenerica>(respuesta, HttpStatus.OK);
     }
-
 
 
     @PostMapping("/CrearPersonal/{cedula}/{id_empresa}")
@@ -108,6 +110,99 @@ public class PersonalEmpresaController {
             estado= HttpStatus.BAD_REQUEST;
         }
         return new ResponseEntity<RespuestaGenerica>(respuesta, estado);
+    }
+    
+    @PutMapping("/EditarPersonal/{cedula}")
+    public ResponseEntity<RespuestaGenerica> EditarPersonal(@RequestBody PersonalEmpresa personalEnviado,@PathVariable String cedula){
+        List<PersonalEmpresa> data = new ArrayList<>();
+        RespuestaGenerica<PersonalEmpresa> respuesta = new RespuestaGenerica<>();
+        AtomicReference<HttpStatus> estado  = new AtomicReference<>(HttpStatus.OK);
+        try {
+            Persona persona = personaRepository.findByCedula(cedula);
+            if(persona!=null){
+                List<PersonalEmpresa> listaPersonal = personalEmpresaRepository.findAll();
+                for (PersonalEmpresa per: listaPersonal) {
+                    if(per.getPersona().getCedula()==persona.getCedula()){
+                        personalEnviado.setEmpresa(per.getEmpresa());
+                        personalEnviado.setPersona(per.getPersona());
+                        personalEnviado.setIdPersonal(per.getIdPersonal());
+                    }
+                }
+                PersonalEmpresa personal = personalEmpresaRepository.findById(personalEnviado.getIdPersonal()).map(res ->{
+                    res.setCargo(personalEnviado.getCargo());
+                    res.setSueldo(personalEnviado.getSueldo());
+                    res.setEmpresa(personalEnviado.getEmpresa());
+                    res.setPersona(personalEnviado.getPersona());
+                    data.add(res);
+                    respuesta.setMensaje("SE MODIFICO EMPLEADO CORRECTAMENTE");
+                    respuesta.setData(data);
+                    respuesta.setEstado(0);
+                    return personalEmpresaRepository.save(res);
+                }).orElseGet(()->{
+                    respuesta.setMensaje("NO SE MODIFICO EL EMPLEADO");
+                    respuesta.setData(data);
+                    respuesta.setEstado(0);
+                    return new PersonalEmpresa();
+                });
+            }else{
+                respuesta.setMensaje("EL EMPLEADO NO PUDO SER MODIFICADO DEBIDO A QUE LA CEDULA -> "+cedula+" NO FUE ENCONTRADA");
+                respuesta.setData(data);
+                respuesta.setEstado(1);
+                estado.set(HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception e){
+            respuesta.setMensaje("Hubo un problema al MODIFICAR EL EMPLEADO, causa ->"+e.getCause()+ " || message -> "+e.getMessage());
+            respuesta.setData(data);
+            respuesta.setEstado(1);
+            estado.set(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<RespuestaGenerica>(respuesta, estado.get());
+    }
+    
+    @DeleteMapping("/EliminarPersonal/{cedula}")
+    public ResponseEntity<RespuestaGenerica> EliminarPersonal(@PathVariable String cedula){
+        List<PersonalEmpresa> data = new ArrayList<PersonalEmpresa>();
+        RespuestaGenerica<PersonalEmpresa> respuesta = new RespuestaGenerica<>();
+        HttpStatus estado  = HttpStatus.OK;
+        try {
+            Persona persona = personaRepository.findByCedula(cedula);
+            if(persona!=null){
+                List<PersonalEmpresa> listaPersonal = personalEmpresaRepository.findAll();
+                PersonalEmpresa personal = new PersonalEmpresa();
+                for (PersonalEmpresa per: listaPersonal) {
+                    if(persona.getCedula()==per.getPersona().getCedula()){
+                        personal=per;
+                    }
+                }
+                if(personal.getPersona().getCedula().equals(cedula)){
+                     personalEmpresaRepository.deleteById(personal.getIdPersonal());
+                     personaRepository.deleteById(persona.getIdPersona());
+                    data.add(new PersonalEmpresa());
+                    respuesta.setMensaje("SE ELIMINO EL EMPLEADO CORRECTAMENTE");
+                    respuesta.setData(data);
+                    respuesta.setEstado(0);
+
+                }else{
+                    data.add(null);
+                    respuesta.setMensaje("EL EMPLEADO NO PUDO SER ELIMINADO DEBIDO A QUE LA CEDULA -> "+cedula+" NO FUE ENCONTRADA ALUM");
+                    respuesta.setData(data);
+                    respuesta.setEstado(1);
+                    estado= HttpStatus.BAD_REQUEST;
+                }
+            }else{
+                data.add(null);
+                respuesta.setMensaje("EL EMPLEADO NO PUDO SER ELIMINADO DEBIDO A QUE LA CEDULA -> "+cedula+" NO FUE ENCONTRADA");
+                respuesta.setData(data);
+                respuesta.setEstado(1);
+                estado= HttpStatus.BAD_REQUEST;
+            }
+        }catch (Exception e){
+            estado= HttpStatus.BAD_REQUEST;
+            respuesta.setMensaje("Hubo un problema al ELIMINAR EL EMPLEADO, causa->"+e.getCause()+ " ||  message -> "+e.getMessage());
+            respuesta.setData(data);
+            respuesta.setEstado(1);
+        }
+        return new ResponseEntity<RespuestaGenerica>(respuesta,estado);
     }
     
     
