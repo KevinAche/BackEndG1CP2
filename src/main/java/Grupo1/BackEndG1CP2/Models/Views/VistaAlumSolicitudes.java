2b878683507e89c684e0d0e7d91cf471df18618a
Kevin Aguilar <kevinv.aguilarl@gmail.com>
2022-06-24 23:37:32 -0500

    NUEVOS CAMPOS PARA VISTA

--- src/main/java/Grupo1/BackEndG1CP2/Models/Views/VistaAlumSolicitudes.java
+++ src/main/java/Grupo1/BackEndG1CP2/Models/Views/VistaAlumSolicitudes.java
@@ -20,6 +20,20 @@ public class VistaAlumSolicitudes {
 
     private String a_apellidos;
 
+    private Long id_solicitud_empresa;
+
+    private Long id_responsableppp;
+
+    private Long cedula_rp;
+
+    private String tit_rp;
+
+    private String rp_nombres;
+
+    private String rp_apellidos;
+
+    private String carrera_rp;
+
     private String estado_solicitud;
 
     private Long id_empresa;
@@ -83,6 +97,62 @@ public class VistaAlumSolicitudes {
 
     public void setNombre_empresa(String nombre_empresa) {
         this.nombre_empresa = nombre_empresa;
+    }
+
+    public Long getId_solicitud_empresa() {
+        return id_solicitud_empresa;
+    }
+
+    public void setId_solicitud_empresa(Long id_solicitud_empresa) {
+        this.id_solicitud_empresa = id_solicitud_empresa;
+    }
+
+    public String getTit_rp() {
+        return tit_rp;
+    }
+
+    public void setTit_rp(String tit_rp) {
+        this.tit_rp = tit_rp;
+    }
+
+    public Long getId_responsableppp() {
+        return id_responsableppp;
+    }
+
+    public void setId_responsableppp(Long id_responsableppp) {
+        this.id_responsableppp = id_responsableppp;
+    }
+
+    public Long getCedula_rp() {
+        return cedula_rp;
+    }
+
+    public void setCedula_rp(Long cedula_rp) {
+        this.cedula_rp = cedula_rp;
+    }
+
+    public String getRp_nombres() {
+        return rp_nombres;
+    }
+
+    public void setRp_nombres(String rp_nombres) {
+        this.rp_nombres = rp_nombres;
+    }
+
+    public String getRp_apellidos() {
+        return rp_apellidos;
+    }
+
+    public void setRp_apellidos(String rp_apellidos) {
+        this.rp_apellidos = rp_apellidos;
+    }
+
+    public String getCarrera_rp() {
+        return carrera_rp;
+    }
+
+    public void setCarrera_rp(String carrera_rp) {
+        this.carrera_rp = carrera_rp;
     }
 
     @Override
